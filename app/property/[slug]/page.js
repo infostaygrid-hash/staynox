8ae6f40e9ab5b8bcbcaf5767c13ea26e4211ab31
StayNox Admin <infostaygrid@gmail.com>
2026-09-18 21:25:23 +0530

    fix: import useEffect in property page

--- app/property/[slug]/page.js
+++ app/property/[slug]/page.js
@@ -1,6 +1,6 @@
 'use client';
 
-import { useState, use } from 'react';
+import { useState, useEffect, use } from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
 import { getPropertyBySlug, getProperties } from '@/app/data/properties';
